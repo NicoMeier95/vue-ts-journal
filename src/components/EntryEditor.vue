787fc052bfd4e01134d3c4f31862d4ff79256cfa
NicoMeier95 <78277049+NicoMeier95@users.noreply.github.com>
2022-10-28 08:52:50 -0300

    Add files via upload

---
+++ src/components/EntryEditor.vue
@@ -0,0 +1,64 @@
+<script lang="ts" setup>
+import EmojiField from "@/components/EmojiField.vue";
+import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
+import type Emoji from "@/types/Emoji";
+import {ref, computed, onMounted, inject} from 'vue'
+import type Entry from "@/types/Entry";
+import { userInjectionKey } from "@/injectionKeys";
+
+
+const body = ref('');
+const emoji = ref<Emoji | null>(null);
+const charCount = computed(()=>body.value.length);
+const maxChars = 280;
+const user = inject(userInjectionKey)
+
+const textarea = ref<HTMLTextAreaElement|null>(null);
+onMounted(() => textarea.value?.focus());
+
+const emit = defineEmits<{
+  (e: '@create', entry:Entry):void;
+}>();
+
+const handleTextInput =(e:Event)=>{
+  const textarea = e.target as HTMLTextAreaElement;
+  if(textarea.value.length <= maxChars){
+    body.value = textarea.value
+  }else{
+    body.value = textarea.value = textarea.value.substring(0,maxChars)
+  }
+}
+
+const handleSubmit = () =>{
+  emit('@create',{
+      body: body.value,
+      emoji: emoji.value,
+      createdAt: new Date(),
+      userid: 1,
+      id: Math.random()
+  });
+
+  body.value = '';
+  emoji.value = null;
+}
+
+</script>
+
+<template>
+  <form 
+    class="entry-form" 
+    @submit.prevent="handleSubmit"
+    >
+    <textarea 
+      :value="body"
+      ref="textarea"
+      @keyup="handleTextInput"  
+     :placeholder="`New Journal Entry for ${user?.username || 'anonymous'}`"
+    ></textarea>
+    <EmojiField v-model="emoji" />
+    <div class="entry-form-footer">
+      <span>{{charCount}} / {{maxChars}}</span>
+      <button>Remember <ArrowCircleRight width="20" /></button>
+    </div>
+  </form>
+</template>
